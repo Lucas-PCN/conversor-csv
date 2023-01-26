a38a5523cca6697c47fa60a785ed4cce8c9af7d8
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-01-26 18:01:55 -0300

    Feat: finaliza projeto

--- src/main/java/com/trybe/conversorcsv/Conversor.java
+++ src/main/java/com/trybe/conversorcsv/Conversor.java
@@ -1,8 +1,17 @@
 package com.trybe.conversorcsv;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
 
+
+/**
+ * Public class Conversor.
+ *
+ */
 public class Conversor {
 
   /**
@@ -30,6 +39,66 @@ public class Conversor {
    *                     gravar os arquivos de saída.
    */
   public void converterPasta(File pastaDeEntradas, File pastaDeSaidas) throws IOException {
-    // TODO: Implementar.
+    if (!pastaDeSaidas.exists()) {
+      pastaDeSaidas.mkdir();
+    }
+
+    if (pastaDeEntradas.isDirectory() && pastaDeEntradas.canRead()) {
+      for (File arquivoEmEntrada : pastaDeEntradas.listFiles()) {
+        File arquivoEmSaida = new File(pastaDeSaidas + "/" + arquivoEmEntrada.getName());
+        alterarArquivo(arquivoEmEntrada, arquivoEmSaida);
+      }
+    }
+  }
+
+  public static void alterarArquivo(File arquivoEmEntrada, File arquivoEmSaida) throws IOException {
+    if (arquivoEmEntrada.exists()) {
+      FileReader fileReader = new FileReader(arquivoEmEntrada);
+      BufferedReader bufferedReader = new BufferedReader(fileReader);
+      
+      FileWriter fileWriter = new FileWriter(arquivoEmSaida);
+      BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+      String linhaLida = bufferedReader.readLine();
+      bufferedWriter.write(linhaLida);
+      bufferedWriter.newLine();
+      bufferedWriter.flush();
+      linhaLida = bufferedReader.readLine();
+
+      while (linhaLida != null) {
+        String[] infosInscrito = linhaLida.split(",");
+
+        String nome = infosInscrito[0].toUpperCase();
+        String data = alterarData(infosInscrito[1]);
+        String email = infosInscrito[2];
+        String cpf = alterarCpf(infosInscrito[3]);
+
+        linhaLida = nome + "," + data + "," + email + "," + cpf;
+
+        bufferedWriter.write(linhaLida);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+        linhaLida = bufferedReader.readLine();
+      }
+
+      fileReader.close();
+      bufferedReader.close();
+      fileWriter.close();
+      bufferedWriter.close();
+    }
+  }
+
+  public static String alterarData(String data) {
+    String[] partes = data.split("/");
+    String dataAlterada = partes[2] + "-" + partes[1] + "-" + partes[0];
+    return dataAlterada;
+  }
+
+  public static String alterarCpf(String cpf) {
+    StringBuilder cpfAlterado = new StringBuilder(cpf);
+    cpfAlterado.insert(3, ".");
+    cpfAlterado.insert(7, ".");
+    cpfAlterado.insert(11, "-");
+    return cpfAlterado.toString();
   }
 }
